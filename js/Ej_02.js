432ba9debaca5c0cdcb592a7ba7153334498bf2d
Anto Ortega <antonellaportega@gmail.com>
2023-10-28 19:36:58 -0300

    correcion de errores

--- js/Ej_02.js
+++ js/Ej_02.js
@@ -9,20 +9,28 @@
 
 // Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
 
-const nota = parseInt(prompt("Ingrese una nota de 0 a 10"));
+const nota = parseFloat(prompt("Ingrese una nota de 0 a 10"));
 
-if (nota>=0 && nota<=2){
-    alert("Muy deficiente");
-}else if (nota>=3 && nota<=4){
-    alert("Insuficiente");
-}else if (nota>=5 && nota<=6){
-    alert("Suficiente");
-}else if (nota===7){
-    alert("Bien");
-}else if (nota>=8 && nota<=9){
-    alert("Notable");
-}else if (nota===10){
-    alert("Sobresaliente");
-}else{
-    document.write("<h1>numero erroneo,introduce un numero valido</h1>");
+if (!isNaN(nota) && nota >= 0 && nota <= 10){
+
+    if (nota>=0 && nota<=2){
+        alert("Muy deficiente");
+    }else if (nota>=3 && nota<=4){
+        alert("Insuficiente");
+    }else if (nota>=5 && nota<=6){
+        alert("Suficiente");
+    }else if (nota===7){
+        alert("Bien");
+    }else if (nota>=8 && nota<=9){
+        alert("Notable");
+    }else if (nota===10){
+        alert("Sobresaliente");
+    }
+}else {
+    // Mostrar mensaje de error si la nota no está en el rango válido
+    if (isNaN(nota)) {
+        alert("Introduce un número válido");
+    } else {
+        alert("Número erróneo. Debe ser un número entre 0 y 10.");
+    }
 }
