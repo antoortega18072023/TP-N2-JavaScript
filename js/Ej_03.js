826e6f389ed3b1656ff580e35422fa102a7a911f
Anto Ortega <antonellaportega@gmail.com>
2023-10-28 19:51:13 -0300

    ejercicio 4 y correcion de errores lexicos

---
+++ js/Ej_03.js
@@ -0,0 +1,13 @@
+// Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+let cadenaConcatenada = "";
+
+do {
+
+    let nuevaCadena = prompt("Ingrese una cadena de texto (o presione 'cancelar' para salir):");
+
+    cadenaConcatenada += nuevaCadena + "-";
+
+} while (confirm("¿Desea ingresar otra cadena de texto?"));
+
+// Mostrar las cadenas concatenadas (sin el último guión)
+alert("Cadenas concatenadas: " + cadenaConcatenada);
