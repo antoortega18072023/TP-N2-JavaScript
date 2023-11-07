ef348edce31a4b4d23a41717349974bf4238ead5
Anto Ortega <antonellaportega@gmail.com>
2023-11-07 12:33:03 -0300

    correcion de numeros dentro dela tabla

--- js/Ej_10.js
+++ js/Ej_10.js
@@ -1,22 +1,20 @@
-// 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+/*
+10- Realiza un script que pida número de filas y columnas y escriba una tabla. 
+Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden 
+descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+*/
 
-const filas=parseInt(prompt("ingese un numero de filas"))
-const columnas=parseInt(prompt("ingrese un numero de columnas"))
+const filas = parseInt(prompt('Ingrese un número de filas'))
+const columnas = parseInt(prompt('Ingrese un número de columnas'))
 
-const totalceldas=filas*columnas;
+let totalCeldas = filas * columnas
 
-document.write(`<table><tbody>`);
-for (let indicefilas=0;indicefilas<filas;indicefilas++){
-    document.write(`<tr>`);
-for (let indicecolumnas=0;indicecolumnas<columnas;indicecolumnas++){
-    document.write(`<td>${totalceldas}</td>`);
-} 
-document.write(`</tr>`);
+document.write(`<table><tbody>`)
+for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
+  document.write(`<tr>`)
+  for (let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++) {
+    document.write(`<td>${totalCeldas--}</td>`)    
+  }
+  document.write(`</tr>`)
 }
-document.write(`</tbody></table>`);
-
-
-
-
-
-)
+document.write(`</tbody></table>`)
