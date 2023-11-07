aa7a856b19bedf3d7afd01818c7e46b87c1b7d57
Anto Ortega <antonellaportega@gmail.com>
2023-11-07 12:48:07 -0300

    ejercicio 13 texto en mayuscula

---
+++ js/Ej_13.js
@@ -0,0 +1,9 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+
+const texto = prompt("ingrese un texto");
+
+const textoEnMayusculas = texto.toUpperCase();
+
+
+document.write("Texto ingresado en mayúsculas: " + textoEnMayusculas);
